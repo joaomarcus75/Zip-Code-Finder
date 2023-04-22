29b3fe962378edeaf53918e8885cec21b31bb4cf
João Marcus <joaomarcus75@gmail.com>
2023-04-22 08:33:20 -0300

    API
    api that searches a brazilian zip code applyed

--- src/App.js
+++ src/App.js
@@ -1,51 +1,65 @@
-import {useState} from 'react'
-import { FiSearch } from 'react-icons/fi'
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
 import './style.css';
 import React from 'react';
 
-function App() {
+import api from './services/api';
 
-  const[input,setInput]= useState('');
-  
-  function handleSearch()
-  {
-    alert("input value " + input);
+function App() {
+  const [input, setInput] = useState('');
+  const [zipcode, setZipCode] = useState({});
+
+  async function handleSearch() {
+   
+
+    if (input === '') {
+      alert("You need to type the zip code...");
+      return;
+    }
+
+    try {
+
+      const response = await api.get(`${input}/json`);
+      setZipCode(response.data)
+      setInput('');
+
+    } catch {
+      alert("There is a problem with your zip code..");
+      setInput('');
+    }
   }
 
   return (
     <div className="container">
       <h1 className="title">Zip Code Finder</h1>
 
-    <div className="containerInput">
-      
-      <input 
-      type="text"
-      placeholder="Type the zip code.."
-      value={input}
-      onChange={(event)=>setInput(event.target.value)}
-      />
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Type the zip code.."
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={25} color="#000" />
+        </button>
+      </div>
 
 
-      <button className="buttonSearch" onClick={handleSearch}>
-      <FiSearch size={25} color="#000"/>
-      </button>
+      {Object.keys(zipcode).length > 0 && (
 
-    </div>
-
-      <main className="main">
-       <h2>Zip Code: 12312123</h2>
-
-       <span>CLOTILDE'S SREETH 41</span>
-       <span>Complement: any thing</span>
-       <span>CHAVO VILLAGE</span>
-       <span>Colorado - USA</span>
-
-      </main>
-
-
+        <main className="main">
+          <h2>Zip Code:{zipcode.cep} </h2>
+          <span>{zipcode.logradouro}</span>
+          <span>Complement: {zipcode.complemento} </span>
+          <span>{zipcode.bairro}</span>
+          <span>Localization: {zipcode.localidade}</span>
+          <span>FU: {zipcode.uf}</span>
+          <span>DDD: {zipcode.ddd}</span>
+        </main>
+      )}
 
     </div>
-    
   );
 }
 
